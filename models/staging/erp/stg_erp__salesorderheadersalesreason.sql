74a1ff0e7edb6c700fe9c7ea10f289ca0f768f32
Eike Hirsch <eikehi@gmail.com>
2025-08-26 00:32:25 +0000

    Creates dim_salesreason table and a bridge table to connect dim_salesreason with the fact_sales

--- models/staging/erp/stg_erp__salesorderheadersalesreason.sql
+++ models/staging/erp/stg_erp__salesorderheadersalesreason.sql
@@ -2,11 +2,11 @@ with
     source_salesorderheadersalesreason as (
     select * 
     from {{ source('erp', 'sales_salesorderheadersalesreason') }}
-),
-
-renamed as (
+)
+, renamed as (
     select
         {{ dbt_utils.generate_surrogate_key(['salesorderid', 'salesreasonid']) }} as sales_order_header_reason_sk
+        , cast(source_salesorderheadersalesreason.salesorderid as int) as salesorder_fk
         , cast(salesreasonid as int) as salesreason_fk
     from source_salesorderheadersalesreason
 )
